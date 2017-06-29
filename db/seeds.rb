bec6c4831da02dc7c768f49969574232124c206e
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 14:38:39 +0000

    added new create and edit buttons for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,8 +22,8 @@ puts "5 skills created"
     subtitle: "My Great Service",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x15"
+    thumb_image: "http://placehold.it/350x200"
     )
-  end
+end
   
-  puts "9 portfolio items created"
+puts "9 portfolio items created"

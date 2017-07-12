665628e59d27c3ae1c84b2dc18e08d5ab78930a0
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 17:45:30 +0000

    Integrated friendly routes for blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621152658) do
+ActiveRecord::Schema.define(version: 20170711174311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170621152658) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
   end
 
   create_table "portfolios", force: :cascade do |t|

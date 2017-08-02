d71ec3ff0efdf29ec2069d787d62abe442c8877e
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 16:43:01 +0000

    Implementd form configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,22 +2,18 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
   end
-  
-  def show
-    @portfolio_item = Portfolio.find(params[:id])
-  end
-  
+
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -27,23 +23,27 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def edit
     @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'Acheivement get! Editor!'}
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
-  
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
